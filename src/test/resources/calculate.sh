5f448bef66c74e14da41e874facfc707bccdd3d6
Morgan Taschuk <mtaschuk@oicr.on.ca>
2014-02-04 14:59:42 -0500

    BWA workflow calculate script; Bartlett workflow changes
    Former-commit-id: eb1565b90603f4766e64527a0ddec3dd5f00c965

--- src/test/resources/calculate.sh
+++ src/test/resources/calculate.sh
@@ -1,10 +1,14 @@
 #/bin/bash
+DIR="$(cd "$( dirname "$0")" && pwd)";
+echo $DIR;
 cd $1
 for i in `find . -type f`; do
 	md5sum $i;
 	if [[ "$i" == *bam ]]
 	    then
-	    LINES=`samtools view ${i} | wc -l`;
+	    #Workflow_Bundle_BWA_1.2_SeqWare_0.13.6.13
+	    SAMTOOLS=$DIR/../Workflow_Bundle_${workflow-directory-name}_${version}_SeqWare_${seqware-version}/Workflow_Bundle_${workflow-directory-name}/${version}/bin/samtools-${samtools-version}/samtools
+	    LINES=`${SAMTOOLS} view ${i} | wc -l`;
 	    if [ $LINES = '0' ];  then echo there are no lines in the BAM file; exit 1; fi
 	fi;
 done
