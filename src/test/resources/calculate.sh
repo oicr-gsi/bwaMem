dbd88a75bc8ac06c268e9efc6d1e485ea6b5b720
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2014-02-06 15:56:10 -0500

    Update workflow-bwa to use workflow-utilities new PATH setting functionality; Update calculate.sh to fail if samtools command is not found
    Former-commit-id: 6b4c4c981db186240f33f1ca6b01998c778e5502

--- src/test/resources/calculate.sh
+++ src/test/resources/calculate.sh
@@ -1,10 +1,15 @@
-#/bin/bash
-cd $1
+#!/bin/bash
+
+set -o pipefail
+
 for i in `find . -type f`; do
-	md5sum $i;
-	if [[ "$i" == *bam ]]
-	    then
-	    LINES=`samtools view ${i} | wc -l`;
-	    if [ $LINES = '0' ];  then echo there are no lines in the BAM file; exit 1; fi
-	fi;
+
+        if [[ "$i" == *bam ]]
+            then
+            LINES=$(samtools view ${i} | wc -l) || exit $?;
+            if [ $LINES = '0' ];  then echo there are no lines in the BAM file; exit 1; fi
+        fi;
+
+        md5sum $i;
+
 done
