8430a5a303835219aa55d1a05d5e1c158de24b5d
pruzanov <pruzanov@oicr.on.ca>
2014-04-04 13:13:02 -0400

    Developing Android Seqprodbio Reporter

--- android/SeqprodReporter/src/ca/on/oicr/pde/seqprodreporter/SeqprodPreferencesFragment.java
+++ android/SeqprodReporter/src/ca/on/oicr/pde/seqprodreporter/SeqprodPreferencesFragment.java
@@ -1,9 +1,12 @@
 package ca.on.oicr.pde.seqprodreporter;
 
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 
-public class SeqprodPreferencesFragment extends PreferenceFragment {
+public class SeqprodPreferencesFragment extends PreferenceFragment 
+    implements Preference.OnPreferenceChangeListener {
 		
 	public SeqprodPreferencesFragment() {
 		super();
@@ -14,6 +17,10 @@ public class SeqprodPreferencesFragment extends PreferenceFragment {
 
 	try {
 	    addPreferencesFromResource(R.xml.preferences);
+	    PreferenceScreen ps = getPreferenceScreen();
+	    for (int i = 0; i < ps.getPreferenceCount(); i++) {
+	      ps.getPreference(i).setOnPreferenceChangeListener(this);
+	    }
 	} catch (Exception e) {
 		//TODO there maybe a conflict with stored SharedPreferences values, 
 		//in this case delete the stored values and try again
@@ -21,5 +28,14 @@ public class SeqprodPreferencesFragment extends PreferenceFragment {
 	}
 
     }
+
+	@Override
+	public boolean onPreferenceChange(Preference preference, Object newValue) {
+		// TODO Auto-generated method stub
+		Preference p = getPreferenceScreen().findPreference(preference.getKey());
+		if (null != p)
+		 p.setSummary("Changed");
+		return true;
+	}
 
 }
