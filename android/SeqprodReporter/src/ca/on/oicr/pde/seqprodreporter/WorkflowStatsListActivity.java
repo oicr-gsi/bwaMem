9faa7c5a53a5fec55fa664273f0a2dc1a2074acd
Ben Reyhani <benreyhani15@hotmail.com>
2014-07-22 17:14:16 -0400

    got rid of un-needed imports

--- android/SeqprodReporter/src/ca/on/oicr/pde/seqprodreporter/WorkflowStatsListActivity.java
+++ android/SeqprodReporter/src/ca/on/oicr/pde/seqprodreporter/WorkflowStatsListActivity.java
@@ -8,8 +8,6 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
-import android.text.format.Time;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
