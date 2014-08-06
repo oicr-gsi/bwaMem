4246846ee7b311bc57c6afb61e688114a167d203
Ben Reyhani <benreyhani15@hotmail.com>
2014-08-06 16:26:22 -0400

    implemented sorting by workflow, sample and last modified time

--- SpbReporterWebApp/public/javascripts/index.js
+++ SpbReporterWebApp/public/javascripts/index.js
@@ -1,5 +1,3 @@
-var async = require('async');
-
 $(document).ready(function(){
 	var hash = window.location.hash;
 
@@ -16,7 +14,155 @@ $(document).ready(function(){
 
 	$('input[name="sortingRadios"]').change(function(){
 		var sortBy = $(this).attr('value');
-		async.parallel([],function(err, sortedLists))
+		var completedItems;
+		var failedItems;
+		var pendingItems;
+
+		if ($('.reportsList#completedReports li.report')){
+			completedItems = $('.reportsList#completedReports li').get();
+		}
+
+		if ($('.reportsList#failedReports li.report')){
+			failedItems = $('.reportsList#failedReports li').get();
+		}
+		if ($('.reportsList#pendingReports li.report')){
+			pendingItems = $('.reportsList#pendingReports li').get();
+		}
+		if (sortBy == 'date'){
+			if (completedItems.length>1){
+				completedItems.sort(function(a,b){
+					var timeOne = a.getElementsByClassName('modified')[0].textContent;
+
+					var timeTwo = b.getElementsByClassName('modified')[0].textContent;
+
+					if (timeOne < timeTwo) return 1;
+					if (timeOne > timeTwo) return -1;
+					return 0;
+				});
+				var completedUList = $('.reportsList#completedReports');
+				$.each(completedItems,function(i, li){
+					completedUList.append(li);
+				});
+			}
+			if (failedItems.length>1){
+				failedItems.sort(function(a,b){
+					var timeOne = a.getElementsByClassName('modified')[0].textContent;
+					var timeTwo = b.getElementsByClassName('modified')[0].textContent;
+
+					if (timeOne < timeTwo) return 1;
+					if (timeOne > timeTwo) return -1;
+					return 0;
+				});
+				var failedUList = $('.reportsList#failedReports');
+				$.each(failedItems,function(i, li){
+					failedUList.append(li);
+				});
+			}
+			if (pendingItems.length>1){
+				pendingItems.sort(function(a,b){
+					var timeOne = a.getElementsByClassName('modified')[0].textContent;
+					var timeTwo = b.getElementsByClassName('modified')[0].textContent;
+
+					if (timeOne < timeTwo) return 1;
+					if (timeOne > timeTwo) return -1;
+					return 0;
+				});
+				var pendingUList = $('.reportsList#pendingReports');
+				$.each(pendingItems,function(i, li){
+					pendingUList.append(li);
+				});
+			}
+		}
+		else if (sortBy == 'workflow'){
+			if (completedItems.length>1){
+				completedItems.sort(function(a,b){
+					var workflowOne = a.getElementsByClassName('workflowName')[0].textContent;
+
+					var workflowTwo = b.getElementsByClassName('workflowName')[0].textContent;
+
+					if (workflowOne < workflowTwo) return -1;
+					if (workflowOne > workflowTwo) return 1;
+					return 0;
+				});
+				var completedUList = $('.reportsList#completedReports');
+				$.each(completedItems,function(i, li){
+					completedUList.append(li);
+				});
+			}
+			if (failedItems.length>1){
+				failedItems.sort(function(a,b){
+					var workflowOne = a.getElementsByClassName('workflowName')[0].textContent;
+					var workflowTwo = b.getElementsByClassName('workflowName')[0].textContent;
+
+					if (workflowOne < workflowTwo) return -1;
+					if (workflowOne > workflowTwo) return 1;
+					return 0;
+				});
+				var failedUList = $('.reportsList#failedReports');
+				$.each(failedItems,function(i, li){
+					failedUList.append(li);
+				});
+			}
+			if (pendingItems.length>1){
+				pendingItems.sort(function(a,b){
+					var workflowOne = a.getElementsByClassName('workflowName')[0].textContent;
+					var workflowTwo = b.getElementsByClassName('workflowName')[0].textContent;
+
+					if (workflowOne < workflowTwo) return -1;
+					if (workflowOne > workflowTwo) return 1;
+					return 0;
+				});
+				var pendingUList = $('.reportsList#pendingReports');
+				$.each(pendingItems,function(i, li){
+					pendingUList.append(li);
+				});
+			}
+		}
+		else if (sortBy == 'sample'){
+			if (completedItems.length>1){
+				completedItems.sort(function(a,b){
+					var sampleOne = a.getElementsByClassName('sampleName')[0].textContent;
+
+					var sampleTwo = b.getElementsByClassName('sampleName')[0].textContent;
+
+					if (sampleOne < sampleTwo) return -1;
+					if (sampleOne > sampleTwo) return 1;
+					return 0;
+				});
+				var completedUList = $('.reportsList#completedReports');
+				$.each(completedItems,function(i, li){
+					completedUList.append(li);
+				});
+			}
+			if (failedItems.length>1){
+				failedItems.sort(function(a,b){
+					var sampleOne = a.getElementsByClassName('sampleName')[0].textContent;
+					var sampleTwo = b.getElementsByClassName('sampleName')[0].textContent;
+
+					if (sampleOne < sampleTwo) return -1;
+					if (sampleOne > sampleTwo) return 1;
+					return 0;
+				});
+				var failedUList = $('.reportsList#failedReports');
+				$.each(failedItems,function(i, li){
+					failedUList.append(li);
+				});
+			}
+			if (pendingItems.length>1){
+				pendingItems.sort(function(a,b){
+					var sampleOne = a.getElementsByClassName('sampleName')[0].textContent;
+					var sampleTwo = b.getElementsByClassName('sampleName')[0].textContent;
+
+					if (sampleOne < sampleTwo) return -1;
+					if (sampleOne > sampleTwo) return 1;
+					return 0;
+				});
+				var pendingUList = $('.reportsList#pendingReports');
+				$.each(pendingItems,function(i, li){
+					pendingUList.append(li);
+				});
+			}
+		}
 	});
 
 	$('li.completed').click(function(){
