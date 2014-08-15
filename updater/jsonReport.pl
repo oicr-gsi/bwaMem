25ee0574b33cf4a125fc99754f63c28f79d163be
pruzanov <pruzanov@oicr.on.ca>
2014-08-15 11:15:56 -0400

    To improve the performance of updateswq made writes to .json files atomic

--- updater/jsonReport.pl
+++ updater/jsonReport.pl
@@ -42,6 +42,8 @@ my $dbhost = 'hsqwprod-db1.hpc.oicr.on.ca';
 my $dbname = 'hsqwprod_seqware_meta_db';
 my $devmode = 0;
 my $timemode= 0;
+my $outfile;
+my $outdir;
 
 
 # Control development output with -d option
@@ -61,25 +63,34 @@ if (exists $ARGV[0])
   {
     $recentCutoffTime = $ARGV[0];
     $recentCutoff = 31 * 24 * 60 * 60;
+    $outfile = "month.json";
   }
   elsif ($ARGV[0] eq "year")
   {
     $recentCutoffTime = $ARGV[0];
     $recentCutoff = 365 * 24 * 60 * 60;
+    $outfile = "year.json";
   }
   elsif ($ARGV[0] eq "decade")
   {
     $recentCutoffTime = $ARGV[0];
     $recentCutoff = 10 * 365 * 24 * 60 * 60;
+    $outfile = "decade.json";
   }
   elsif ($ARGV[0] ne "week")
   {
-    die "Please run as seqwareJsonReport.pl [week|month|year|decade] > output.html\n";
+    die "Please run as seqwareJsonReport.pl [week|month|year|decade] [output_dir]\n";
   }
   # defaults are fine
 }
 
+if (exists $ARGV[1] && -d $ARGV[1]) {
+  $outdir = $ARGV[1];
+} else {
+  die "Need an output directory for writing into";
+}
 
+$outfile ||= "week.json";
 warn "Connecting to meta_db\n";
 
 my $dsn = "DBI:Pg:dbname=$dbname;host=$dbhost";
@@ -193,12 +204,22 @@ map{print STDERR $_."\n"} (keys %results) if DEBUG;
 
 my $json = JSON->new();
 $json = encode_json \%results;
-print "$json\n";
+
+# Atomic update
+$outdir.="/" if $outdir !~m!/$!;
+my $tmppath = $outdir."tmp.json_$$";
+open(TEMP,">$tmppath") or die "Couldn't write to a temporary file";
+print TEMP "$json\n";
+close TEMP;
+
+my $outpath =$outdir.$outfile;
+`mv $tmppath $outpath`;
 
 # Returns time in seconds
 sub parse_timestamp {
  my $tmpstmp = shift @_;
  if ($tmpstmp =~ /^(....)-(..)-(..) (..):(..):(..)/) {
+ print STDERR $tmpstmp."\n" if DEBUG;
                 my $year = $1;
                 my $mon = $2 - 1;
                 my $mday = $3;
