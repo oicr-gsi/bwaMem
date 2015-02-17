afb5072b505f60dfa3ed4a14918a7536afbeae04
Junyan Chen <jchen@Junyan.Chen@oicr.on.ca>
2015-02-17 12:15:56 -0500

    ..

---
+++ SpbReporterWebApp_configured/routes/index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var Report = require('../models/reports');
+var async = require('async');
+/* GET home page. */
+router.get('/', function(req, res) {
+	async.series([getCompletedReports,getFailedReports,getPendingReports],
+		function(err, reportsArray){
+			res.render('index',{completed_reports:reportsArray[0], failed_reports:reportsArray[1], pending_reports:reportsArray[2], JSONdownloadTime:downloadTime});
+		});
+
+});
+module.exports = router;
+
+function getCompletedReports(callback){
+	Report.find({workflow_run_type:'completed'}).sort({last_modified_date : 'desc'}).exec(function(err, completed_reports){
+		console.log('completed: ' + completed_reports.length);
+
+		if (err){
+			callback(err, null);
+		}
+		else {
+			callback(null, completed_reports);
+		}
+	});
+}
+
+function getFailedReports(callback){
+	Report.find({workflow_run_type:'failed'}).sort({last_modified_date : 'desc'}).exec(function(err, failed_reports){
+		console.log('failed: ' + failed_reports.length);
+		if (err){
+			callback(err, null);
+		}
+		else {
+			callback(null, failed_reports);
+		}
+	});
+}
+
+function getPendingReports(callback){
+	Report.find({workflow_run_type:'pending'}).sort({last_modified_date : 'desc'}).exec(function(err, pending_reports){
+		console.log('pending: ' + pending_reports.length);
+		if (err){
+			callback(err, null);
+		}
+		else {
+			callback(null, pending_reports);
+		}
+	});
+}
---
+++ SpbReporterWebApp_configured/startServer.sh
@@ -0,0 +1,10 @@
+
+> SpbReporterWebApp@0.0.1 start /.mounts/labs/PDE/web/webapp-utils/SpbReporterWebApp
+> node ./bin/www
+
+server started
+connected to DB
+completed: 8743
+failed: 7
+pending: 357
+finished downloading new data to DB
